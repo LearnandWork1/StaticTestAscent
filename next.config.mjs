d1a9a66c75c37086f6a143832705597c1c9efb79
Srilakshmi Dotnet <srilakshmich.as@gmail.com>
2024-12-16 11:55:25 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+// next.config.mjs
+export default {
+    output: 'export',  // Enables static export
+    assetPrefix: './', // Necessary for GitHub Pages
+  };
+  
